a4fe946cf735c9ecffcefc9cb85e77e4c15cc323
Catiane Lima Viana <katiane20@gmail.com>
2022-07-14 17:06:38 -0400

    Update math_test.go

--- math_test.go
+++ math_test.go
@@ -1,6 +1,6 @@
 package main
 	
-func TestSoma(t * testing.T) {
+func TestSoma((t *testing.T) {
 	total := Soma(15,15)
 	if total != 30 {
 		t.Fatalf("Resultado da Soma é  invalido: Resultado %v. Esperado %v", total, 30)
