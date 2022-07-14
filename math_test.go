d70e6d75fa2d43b905db0096577f84022c6b2f1f
Catiane Lima Viana <katiane20@gmail.com>
2022-07-14 14:43:50 -0400

    Update math_test.go
    fix bug

--- math_test.go
+++ math_test.go
@@ -1,8 +1,13 @@
 package main
+	
+import (
+    "fmt"
+    "testing"
+)
 
 func TestSoma(t * testing.T){
 	total := Soma(15,15)
 	if total!=30{
-		t.Errorf("Resultado da Soma é  invalido: Resultado %d. Esperado %d", total, 30)
+		t.Fatalf("Resultado da Soma é  invalido: Resultado %v. Esperado %v", total, 30)
 	}
 }
