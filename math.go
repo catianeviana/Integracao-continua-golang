895451391b021f1a6c08985e5cac930f272f3aa5
Catiane Lima Viana <katiane20@gmail.com>
2022-07-14 17:29:53 -0400

    Update math.go

--- math.go
+++ math.go
@@ -1,12 +1,12 @@
 package main
-	
-import (
-    "fmt"
-)
-func main(){
-	fmt.Println(Soma(10,10))
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(112, 10))
 }
 
-func Soma(a int, b int) int {
-	return a+b
+func soma(a int, b int) int {
+	return a + b
 }
+
